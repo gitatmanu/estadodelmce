8f15bfc0799fa1045f6ef566a5f40f5bb161cba0
manel <gitatmanu@gmail.com>
2021-07-13 19:30:53 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+python manage.py check --deploy
+
+
 # Make migrations and migrate the database.
 echo "Making migrations and migrating the database. "
 python manage.py makemigrations main --noinput 
